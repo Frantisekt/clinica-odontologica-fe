3f720076e8c899122eb01ceb385c5f78167aaf3c
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-12-21 00:10:58 -0500

    refactor: moved all routes to an AppRoutes component.
    Just changed this to correctly show the new AppRoutes component, now, the routes will be handled over there.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './routes/Home';
-import LoginPage from './pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-//import PatientList from './pages/patient/PatientList';
-//import PatientAdd from './pages/patient/PatientAdd';
-//import DentistList from './pages/dentist/DentistList';
-//import DentistAdd from './pages/dentist/DentistAdd';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
@@ -15,15 +9,7 @@ function App() {
       <div className="app-container">
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            {/* <Route path="/pacientes" element={<PatientList />} />
-            <Route path="/pacientes/agregar" element={<PatientAdd />} />
-            <Route path="/odontologos" element={<DentistList />} />
-            <Route path="/odontologos/agregar" element={<DentistAdd />} /> */}
-          </Routes>
+          <AppRoutes />
         </main>
         <Footer />
       </div>
